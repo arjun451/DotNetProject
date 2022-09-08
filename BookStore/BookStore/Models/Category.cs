2e30adaed63f37550d8dd0ccec1c29b3d092a7ec
arjun451 <aksingh25071998@gmail.com>
2022-09-08 13:16:18 +0530

    init project

---
+++ BookStore/BookStore/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+       [Required]
+        public string Name { get; set; }
+        [Required]
+        public int DisplayOrder { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfCreation { get; set; } = DateTime.Now;
+
+    }
+}
