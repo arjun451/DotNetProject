38addeb34b3daea5392d3682f74f41dde38e670a
arjun451 <aksingh25071998@gmail.com>
2022-09-09 09:47:50 +0530

    Bootswatch Theam added

--- BookStore/BookStore/Controllers/CategoryController.cs
+++ BookStore/BookStore/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> obj = _db.Categories.ToList();
+            return View(obj);
         }
     }
 }
