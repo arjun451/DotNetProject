b015c1a16786c1ec54d36041271136985eec370e
arjun451 <aksingh25071998@gmail.com>
2022-09-12 11:08:16 +0530

    init Razor

---
+++ RazorProject/RazorProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorProject.Model;
+
+namespace RazorProject.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> categories { get; set; }
+    }
+}
