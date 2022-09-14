ef479c48e30e66020e6d09c15476cefd7417ebe2
arjun451 <aksingh25071998@gmail.com>
2022-09-14 12:17:32 +0530

    Area concept add

--- BookStore/BookStore/Areas/Admin/Controllers/CategoryController.cs
+++ BookStore/BookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -3,15 +3,16 @@ using BookStore.DataAccess.Repository.IRepository;
 using BookStore.Model.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookStore.Controllers
+namespace BookStore.UserInterface.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUniteOfWork _db;
 
         public CategoryController(IUniteOfWork db)
         {
-            this._db = db;
+            _db = db;
         }
         [HttpGet]
         public IActionResult Index()
@@ -22,13 +23,13 @@ namespace BookStore.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-             
+
             return View();
         }
         [HttpPost]
-        public IActionResult Create( Category obj)
+        public IActionResult Create(Category obj)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _db.Category.Add(obj);
                 _db.Save();
@@ -40,17 +41,17 @@ namespace BookStore.Controllers
         [HttpGet]
         public IActionResult Edit(int id)
         {
-            var obj = _db.Category.GetFirstOrDefault(u=>u.Id==id);
-            if(id==0||obj==null)
+            var obj = _db.Category.GetFirstOrDefault(u => u.Id == id);
+            if (id == 0 || obj == null)
             {
                 return NotFound();
             }
             return View(obj);
         }
         [HttpPost]
-        public IActionResult Edit( Category obj)
+        public IActionResult Edit(Category obj)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _db.Category.Update(obj);
                 _db.Save();
@@ -62,8 +63,8 @@ namespace BookStore.Controllers
         [HttpGet]
         public IActionResult Delete(int id)
         {
-            var obj = _db.Category.GetFirstOrDefault(u=>u.Id == id);
-            if(obj==null)
+            var obj = _db.Category.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
             {
                 return NotFound();
             }
@@ -84,7 +85,7 @@ namespace BookStore.Controllers
                 TempData["success"] = "Category Delete Successfully ";
                 return RedirectToAction("Index");
             }
-             
+
         }
     }
 }
--- BookStore/BookStore/Areas/Customer/Controllers/HomeController.cs
+++ BookStore/BookStore/Areas/Customer/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace BookStore.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
